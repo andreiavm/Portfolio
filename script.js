c99ebddfc2d03b5b5915c5b05741dfc557f515e5
Andrei A <64260017+andreiavm@users.noreply.github.com>
2021-11-26 21:09:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
 
   var timeline = new TimelineMax();
   timeline.from("#header", 1.25, {opacity: 0, y:-25, ease: Power3.easeOut},0)
-  timeline.from("#yellow-rect", 2, {opacity: 0, x:470, ease: Power3.easeOut},0)
+  timeline.from("#yellow-rect", 2, {opacity: 0, x:500, ease: Power3.easeOut},0)
   timeline.from("#hero-left", 1.5, {opacity:0, x:-50, ease: Power3.easeOut},0)
   timeline.from("#abstract-shapes", 2, {opacity:0, ease: Power3.easeOut},0)
 }
