1c48ee80c116f7e83bc584d0f267e66aa419d5ba
Andrei A <64260017+andreiavm@users.noreply.github.com>
2021-12-15 23:35:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ window.onload = function() {
   timeline.from("#yellow-rect", 1.25, {opacity: 0, x:430, ease: Power3.easeOut},0)
   timeline.from("#hero-left", 1.25, {opacity:0, x:-50, ease: Power3.easeOut},0)
   timeline.from("#abstract-shapes", 1.5, {opacity:0, ease: Power3.easeOut},0)
+  timeline.from(".work", 1.5, {opacity:0,y:-25, ease: Power3.easeOut},0)
+  timeline.from(".about", 1.5, {opacity:0,y:-25, ease: Power3.easeOut},0)
 }
 }
 var tl = new TimelineMax({onUpdate:updatePercentage});
-var tl2 = new TimelineMax();
+// var tl2 = new TimelineMax({onUpdate:updatePercentage});
 const controller = new ScrollMagic.Controller();
 
 tl.from("#side-menu", 0.5, {x:-50, opacity: 0, ease: Power1.easeOut})
